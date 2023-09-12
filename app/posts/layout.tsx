a9f71547cb42ff47e0dec58b72b70ffa3689f8a0
Kirill Pulnev <kirill_pulnev@senla.eu>
2023-09-12 17:44:40 +0300

    Init

---
+++ app/posts/layout.tsx
@@ -0,0 +1,11 @@
+export default function PostsLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <main className="flex min-h-screen">
+            {children}
+        </main>
+    )
+}
---
+++ app/posts/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <h1>Posts page!</h1>
+    )
+}
