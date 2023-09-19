b4edf35568a32e088e056bc1c24a6caadb86dfb4
bybbsys <crysis099@mail.ru>
2023-09-19 16:18:22 +0300

    Error item

---
+++ app/components/error/error.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+import { ErrorItemProps } from "./types/types"
+
+export function ErrorItem({ message, extraInfo }: ErrorItemProps) {
+  return (
+    <div className="flex flex-col items-center py-2">
+      <h1 className="text-5xl font-bold">{message}</h1>
+      {extraInfo && <span className="mt-10 text-xl">{extraInfo}</span>}
+      <p className="mt-10">
+        <Link href='/' className="text-xl underline underline-offset-8 text-gray-600 hover:text-gray-500">Go to main page</Link>
+      </p>
+    </div>
+  )
+}
---
+++ app/components/error/types/types.ts
@@ -0,0 +1,4 @@
+export type ErrorItemProps = {
+  message: string,
+  extraInfo?: string,
+}
